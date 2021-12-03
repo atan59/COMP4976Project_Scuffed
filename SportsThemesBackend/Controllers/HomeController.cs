9fa1e2eede8ca8d75277c70968133b945a061b51
Amy Tan <atan59@my.bcit.ca>
2021-12-02 20:21:05 -0800

    [Amy] Added text for home page. -Amy

--- SportsThemesBackend/Controllers/HomeController.cs
+++ SportsThemesBackend/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using SportsThemesBackend.Data;
 using SportsThemesBackend.Models;
 using System;
 using System.Collections.Generic;
@@ -9,17 +12,29 @@ using System.Threading.Tasks;
 
 namespace SportsThemesBackend.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var themes = await _context.Themes
+                .ToListAsync();
+
+            var teams = await _context.Teams
+                .ToListAsync();
+
+            ViewBag.NumOfThemes = themes.Count();
+            ViewBag.NumOfTeams = teams.Count();
+
             return View();
         }
 
