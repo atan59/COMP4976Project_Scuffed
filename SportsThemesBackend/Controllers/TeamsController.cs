b170887b764852038bc5199ec28ec4e4d2fa6be6
Amy Tan <atan59@my.bcit.ca>
2021-12-02 23:00:09 -0800

    [Amy] Fixed bug creating a team with a duplicate name. -Amy

--- SportsThemesBackend/Controllers/TeamsController.cs
+++ SportsThemesBackend/Controllers/TeamsController.cs
@@ -119,7 +119,6 @@ namespace SportsThemesBackend.Controllers
         public async Task<IActionResult> Create([Bind("TeamName, City, CoachId, ThemeId")] Team team)
         {
             var currentTeams = await _context.Teams
-                .Where(t => t.TeamName != team.TeamName)
                 .ToListAsync();
 
             foreach (var currentTeam in currentTeams)
